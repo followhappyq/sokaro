5a8885cacc9e1826779ae0e5ce0b1fa2dd174219
Ivan <followhappyq@gmail.com>
2020-05-11 21:49:14 +0200

    Updates

---
+++ precache-manifest.a7102b72f9178391609e34cd61cc2efd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ae75b1cdf36bac90b802901fdf4f57",
+    "url": "/sokaro/index.html"
+  },
+  {
+    "revision": "b8aca4b429e2003ced1d",
+    "url": "/sokaro/static/css/main.92708fb0.chunk.css"
+  },
+  {
+    "revision": "1b11d4233e2ca8945c52",
+    "url": "/sokaro/static/js/2.705bd4c9.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/sokaro/static/js/2.705bd4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8aca4b429e2003ced1d",
+    "url": "/sokaro/static/js/main.ad5636c8.chunk.js"
+  },
+  {
+    "revision": "d848d476d36b88c03282",
+    "url": "/sokaro/static/js/runtime-main.53dd6df4.js"
+  },
+  {
+    "revision": "2dad7c0757ffb3a464c561efebca19d3",
+    "url": "/sokaro/static/media/header-background.2dad7c07.png"
+  }
+]);
